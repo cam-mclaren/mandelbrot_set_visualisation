4c2e41994f58a60cbcc4d7572509c5090b8d60d1
DataCruncherCameron <86663659+DataCruncherCameron@users.noreply.github.com>
2021-10-08 22:46:01 +1100

    I have coded up a multiplier array and the center coordinates arrays for the zoom file. I have also coded up the base arrays that will be scaled and translated so to make the x and y values for the frames.
    PROBLEM MAYBE?
    I am worried that my growth multiplier values are not linear...
    or more accurately are linear. I think the zoom may become
    clunkier as the zoom approaches the final frame. I have a potential
    solution to this involving a while loop and a growth factor to select x values.
    The distance the center moves from the first center to the final should shrink
    with each frame.

--- zoom/zoom.c
+++ zoom/zoom.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char * * argv)
 {
-  // Coordinate to zoom in upon.
+  // Coordinate to zoom in upon. (Centre of the final frame)
   double x;
   double y;
 
@@ -17,12 +17,7 @@ int main(int argc, char * * argv)
   double right =2;
 
   double first_frame_width = right - left;
-  int n
 
-  // initialise arrays for the frame specs. We will unpack these backwards starting from n-1 and going to 0
-  double * c_x=malloc(n*sizeof(double)); // centre x coordinate
-  double * c_y= malloc(n*sizeof(double)); // centre y coordinate
-  double * multiplier = malloc(n*sizeof(double));  // Multiplier of the desired final frame width
 
   //Pixel data for image
 
@@ -36,8 +31,12 @@ int main(int argc, char * * argv)
   double top = aspect_ratio*right;
   double bot = aspect_ratio*left;
 
-  double * closeness = malloc(4*sizeof(double));
+  // Now that we have both the horizontal and vertical boundaries we can find the centre of the first frame.
+  double first_frame_centre_x = (Right+Left)/2;
+  double first_frame_centre_y = (Top+Bottom)/2;
 
+  // Define an array to see how big we can make the final frame so that it touches only one boundary
+  double * closeness = malloc(4*sizeof(double));
   closeness[0]=x-left;
   closeness[1]=right-x;
   closeness[2]=(y-bot)/aspect_ratio;
@@ -52,7 +51,54 @@ int main(int argc, char * * argv)
     if(closeness[i]<current_min)
     {
       current_min=closeness[i];
-      current_index=i; 
+      current_index=i;
     }
   }
+
+
+// We can now calculate the width of the final frame
+double final_frame_width = 2*current_min;
+
+int n = floor(log(first_frame_width/final_frame_width)/log(1.025))+1;
+
+
+// initialise arrays for the frame specs. We will unpack these backwards starting from n-1 and going to 0
+double * c_x=malloc(n*sizeof(double)); // centre x coordinate
+double * c_y= malloc(n*sizeof(double)); // centre y coordinate
+double * multiplier = malloc(n*sizeof(double));  // Multiplier of the desired final frame width
+
+// We can define the gradient parametes of the 2 line equations
+double gradient_y = (first_frame_centre_y-y)/(first_frame_centre_x-x);
+double gradient_mult = ((first_frame_width-final_frame_width)/(first_frame_centre_x-x);
+
+// Populate frame parameter arrays
+for(i = 0 ; i < n; i++)
+{
+  c_x[i] = x+ (first_frame_centre_x-x)/(n-1)*i;
+  c_y[i] = gradient_y*(c_x[i]-x)+y;
+  multiplier[i] = gradient_mult*(c_x[i]-x)+final_frame_width;
+}
+
+
+double * base_x_values=malloc(x_pixel_count*sizeof(double));
+double * base_y_values=malloc(y_pixel_count*sizeof(double));
+
+// Populate the base values
+
+
+for( i = 0; i <x_pixel_count; i++)
+{
+  base_x_values[i]= -0.5 + (1)/(x_pixel_count-1)*i;
+}
+for( i = 0; i <y_pixel_count; i++)
+{
+  base_y_values[i]= -0.5*aspect_ratio + (1*aspect_ratio)/(y_pixel_count-1)*i;
+}
+
+
+
+
+
+
+
 }
