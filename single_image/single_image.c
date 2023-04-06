fbdcb2dfb54f46547ea6cd707927c62103ff108c
DataCruncherCameron <DataCruncherCameron@gmail.com>
2023-04-07 00:16:14 +1000

    Have got the nested loop on the y and x axis working in the worker_function.

--- single_image/single_image.c
+++ single_image/single_image.c
@@ -62,68 +62,79 @@ int worker_function( void * thread_arg /* void pointer to int */)
 	mpfr_init2(current_y,MY_PRECISION);
 	mpfr_init2(current_x,MY_PRECISION);
 
-
-
-
-
 	image_params *args = (image_params*)thread_arg;
 
 
-	mtx_lock(args->mutex_ptr);
-	current_row = *(args->row_count); // read current row
+	int count = 0;
+	printf("args->ypixels = %d\n", args->y_pixels);
+	while (count < args->y_pixels)
+	{
+		
 
-	if (current_row >= args->y_pixels)
-	{	
-		printf("Worker function is returning\n");
-		return 0;
+
+		mtx_lock(args->mutex_ptr);
+		current_row = *(args->row_count); // read current row
+		printf("current row = %d\n", current_row);
+		if (current_row == args->y_pixels -1)
+		{	
+			printf("Worker function is returning\n");
+			return 0;
+		} else
+		{
+			*(args->row_count) = current_row+1;
+			printf("incrementing\n");
+		}
+		mtx_unlock(args->mutex_ptr);
+
+		mpfr_set(current_y, args->y_array[current_row], MPFR_RNDD);
+
+		mpfr_fprintf(stdout,"%5.50Rf\n",current_y);
+		printf("Writing X values from worker function\n");
+		int j;
+		for(j = 0; j< args->x_pixels; j++)
+				{	
+
+					//mpfr_fprintf(stdout,"%5.50Rf\n",args->x_array[j]);
+					// mpfr_set(current_x, args->x_array[j], MPFR_RNDD);
+					// mpfr_set(real_component, current_x, MPFR_RNDD); //  Initialise x value
+					// mpfr_set(imaginary_component, current_y, MPFR_RNDD); // Initialise y value
+					// run time escape algorithm
+					// inner_product=0;
+					// iter_count=0;
+					// while( (iter_count < max_iter)&&(inner_product<4))
+					// {
+						// f(z) = z^2 + c
+						// (x+yi)(x+yi) = x^2 - y^2 + (2xy)i
+						//Real component calculation store in real_temp
+						// mpfr_pow_si(x_square, real_component, 2, MPFR_RNDD);
+						// mpfr_pow_si(y_square, imaginary_component, 2, MPFR_RNDD);
+						// mpfr_sub(real_temp, x_square, y_square, MPFR_RNDD);
+						//imaginary_component
+						// mpfr_mul(imaginary_component, imaginary_component, real_component, MPFR_RNDD);
+						// mpfr_mul_si(imaginary_component, imaginary_component, 2, MPFR_RNDD); // imaginary component calculate
+						// add c
+						// mpfr_add(real_component, real_temp, current_x, MPFR_RNDD);
+						// mpfr_add(imaginary_component, imaginary_component, current_y, MPFR_RNDD);
+						//find the inner product
+						// mpfr_pow_si(x_square, real_component, 2, MPFR_RNDD);
+						// mpfr_pow_si(y_square, imaginary_component, 2, MPFR_RNDD);
+						// mpfr_add(real_temp, x_square, y_square, MPFR_RNDD);
+						// inner_product=mpfr_get_d(real_temp, MPFR_RNDD);
+
+						//if(iter_count%10==0)
+						//{
+							//printf("iter_count=%d, i=%d , j=%d \n",iter_count, i, j);
+						//}
+						// iter_count++;
+
+					// }
+
+
+				}
+		count++;
 	}
-	mtx_unlock(args->mutex_ptr);
-
-	mpfr_set(current_y, args->y_array[current_row], MPFR_RNDD);
-
-
-	printf("Writing X values from worker function\n");
-	int j;
-	for(j = 0; j< args->x_pixels; j++)
-			{	
-
-				mpfr_fprintf(stdout,"%5.50Rf\n",args->x_array[j]);
-				// mpfr_set(current_x, args->x_array[j], MPFR_RNDD);
-				// mpfr_set(real_component, current_x, MPFR_RNDD); //  Initialise x value
-				// mpfr_set(imaginary_component, current_y, MPFR_RNDD); // Initialise y value
-				// run time escape algorithm
-				// inner_product=0;
-				// iter_count=0;
-				// while( (iter_count < max_iter)&&(inner_product<4))
-				// {
-					// f(z) = z^2 + c
-					// (x+yi)(x+yi) = x^2 - y^2 + (2xy)i
-					//Real component calculation store in real_temp
-					// mpfr_pow_si(x_square, real_component, 2, MPFR_RNDD);
-					// mpfr_pow_si(y_square, imaginary_component, 2, MPFR_RNDD);
-					// mpfr_sub(real_temp, x_square, y_square, MPFR_RNDD);
-					//imaginary_component
-					// mpfr_mul(imaginary_component, imaginary_component, real_component, MPFR_RNDD);
-					// mpfr_mul_si(imaginary_component, imaginary_component, 2, MPFR_RNDD); // imaginary component calculate
-					// add c
-					// mpfr_add(real_component, real_temp, current_x, MPFR_RNDD);
-					// mpfr_add(imaginary_component, imaginary_component, current_y, MPFR_RNDD);
-					//find the inner product
-					// mpfr_pow_si(x_square, real_component, 2, MPFR_RNDD);
-					// mpfr_pow_si(y_square, imaginary_component, 2, MPFR_RNDD);
-					// mpfr_add(real_temp, x_square, y_square, MPFR_RNDD);
-					// inner_product=mpfr_get_d(real_temp, MPFR_RNDD);
-
-					//if(iter_count%10==0)
-					//{
-						//printf("iter_count=%d, i=%d , j=%d \n",iter_count, i, j);
-					//}
-					// iter_count++;
-
-				// }
-
-
-			}
+	fprintf(stderr, "Thread function loop maxed out");
+	return -99;
 }
 // Instead maybe it reads targets from a file.
 
@@ -258,7 +269,7 @@ int main(void)
 		mpfr_sub(y_val, top, y_increment, MPFR_RNDD);
 		mpfr_init2(y_values[index], MY_PRECISION);
 		mpfr_set(y_values[index], y_val, MPFR_RNDD);
-		mpfr_fprintf(stdout,"%5.50Rf\n",y_values[index]);
+		// mpfr_fprintf(stdout,"%5.50Rf\n",y_values[index]);
 	}
 	// Populate x values used. (Not yet utilised)
 	mpfr_t x_val, x_increment;
